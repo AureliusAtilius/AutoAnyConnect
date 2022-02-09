c917a72972076464af9c9d8364dbafc4cbf3b849
jlindner <josh.lindner@karemaircraft.com>
2022-02-09 12:17:19 -0800

    Add Sikuli functionality for clicking the "Connect" button on the login prompt.
    Add .bat file for startup folder.

--- AutoAnyConnect.py
+++ AutoAnyConnect.py
@@ -2,14 +2,15 @@ import ifcfg, requests,subprocess
 
 
 
-def connectionCheck():
-    
+def connectionCheck(ext_domain):
+    #Get network information
     interfaces = ifcfg.interfaces()
     result= False
+    #Check which network adapter is connected. If Wifi is connected then check connectivity to external domain.
     try:
 
         if (not interfaces['Ethernet adapter Ethernet']['inet'] and interfaces['Wireless LAN adapter Wi-Fi']['inet']!= None):
-            request=requests.get("https://karemaircraft.com", timeout=3)
+            request=requests.get(ext_domain, timeout=3)
             result = True
     except KeyError:
         print("Check Adapter Names")
@@ -18,11 +19,28 @@ def connectionCheck():
     return result
 
 def openproc(exe_path):
+    #Open vpn client
     try:
         subprocess.Popen(exe_path)
     except FileNotFoundError:
         print("Unable to locate process.")
-    finally: 
-        print("Could not open process.")
+    
 
+
+def runsikuli(ide_path, script_path):
+    #Run Sikuli script to click "Connect" button.
+    sikuli_path=[ide_path,"-r",script_path]
+    try:
+        subprocess.Popen(sikuli_path, shell=True)
+    except FileNotFoundError:
+        print("Unable to locate script.")
+    
+
+
+
+if __name__=='__main__':
+    if connectionCheck("https://google.com"):
+        openproc("C:\\Program Files (x86)\\Cisco\\Cisco AnyConnect Secure Mobility Client\\vpnui.exe")
+        runsikuli("C:\\Users\\jlindner\\SikuliX\\sikulixide-2.0.5.jar","C:\\Users\\jlindner\\Repos\\AutoAnyConnect\\click_connect.sikuli")
+    
 
